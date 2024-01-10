4d2e9d1474ea208b17fa371da47eecba4803c03f
zerlix <t.hemm@t-online.de>
2024-01-10 16:29:04 +0100

    Projektname Dialog hinzugefügt

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
 from PySide6.QtCore import Qt 
 from PySide6.QtWidgets import QApplication, QMainWindow
 from src.MainWindow_ui import Ui_MainWindow
-        
+from src.DialogProjectName_ui import Ui_DialogProjectName
 
 
+
+
+''' Klasse die den Dialog zum editieren des Projekt Name erstellt '''        
+class DialogProjectName(QMainWindow, Ui_DialogProjectName):
+    def __init__(self):
+        super(DialogProjectName, self).__init__()
+        self.setupUi(self)
+
+
+
+
+''' Klasse die das Hauptfenster erzeugt '''
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -18,13 +30,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
 
-
+''' Hauptprogramm '''
 if __name__ == "__main__":
     # Mainwindow
     app = QApplication(sys.argv)
 
     main_Window = MainWindow()
     main_Window.show()
+    widget_EditProjectName = DialogProjectName()
+    
     
 
     sys.exit(app.exec())
+
