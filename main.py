dcf3528c49625079512032e76ad14c659d9c290b
zerlix <t.hemm@t-online.de>
2024-01-13 13:10:58 +0100

    comments added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import os
 
-# Setzen Sie QT_IM_MODULE auf 'qtvirtualkeyboard'
+'Virtuelle Tastatur'
 #os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
 
 from PySide6.QtCore import Qt
@@ -11,54 +11,49 @@ from src.ui_MainWindow import Ui_MainWindow
 from src.DialogProjectName import DialogProjectName
 
 
- 
-  
-
-
-
 class MainWindow(QMainWindow, Ui_MainWindow):
     ''' Klasse die das Hauptfenster erzeugt '''
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.setWindowFlag(Qt.FramelessWindowHint) 
-        #self.showFullScreen()
+        'self.showFullScreen()'
 
-        # dialog fenster projektname
+        'dialog fenster projektname'
         self.pushButtonProjektname.clicked.connect(self.openDialogProjectName)
         self.dialogProjectName = DialogProjectName(self)
         
-        # M2 Resetbutton
+        'm² Resetbutton'
         self.m2pushButton.clicked.connect(self.M2ResetClicked)
         
+
     'Methode zum öffnen des Dialogs zum editieren des Projektnamens '
     def openDialogProjectName(self):
         self.dialogProjectName.show()
 
-    'M2 Resetbutton clicked'    
+
+    'm² Resetbutton clicked'    
     def M2ResetClicked(self):
         self.resetM2Lcd()
-    
-    'setze LCD zurück'
+
+
+    'setze m² LCD zurück'
     def resetM2Lcd(self):
         self.m2lcdNumber.display(0)
 
 
-
-
-
-# Hauptprogramm starten
+'Hauptprogramm starten'
 if __name__ == "__main__":
 
-    # QApplication initialisieren
+    'QApplication initialisieren'
     app = QApplication(sys.argv)
 
-    # Stylesheet einlesen und anwenden
+    'Stylesheet einlesen und anwenden'
     with open('./styles.qss', 'r') as f:
         stylesheet = f.read()
     app.setStyleSheet(stylesheet) 
 
-    # Hauptfenster anzeigen    
+    'Hauptfenster anzeigen'    
     main_Window = MainWindow()
     main_Window.show()
     sys.exit(app.exec())
