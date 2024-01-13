492b91d15423cea10b9b35897204689e5c63fdf1
zerlix <t.hemm@t-online.de>
2024-01-13 13:39:32 +0100

    little changes

--- ui_compile.py
+++ ui_compile.py
@@ -10,6 +10,6 @@ src_dir = './src'
 if __name__ == "__main__":
     for filename in os.listdir(ui_dir):
         if filename.endswith('.ui'):
-            print(ui_dir+'/'+filename)
-            print(src_dir+'/ui_'+filename[:-3  ]+'.py')
+            print('Compiling... '+ui_dir+'/'+filename)
+            print('Save...      '+src_dir+'/ui_'+filename[:-3  ]+'.py')
             os.system('pyside6-uic '+ui_dir+'/'+filename+' -o '+src_dir+'/ui_'+filename[:-3]+'.py')
