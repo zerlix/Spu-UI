3eb210652640a0c2fdd93cf241c3f1c4f16a06ec
zerlix <t.hemm@t-online.de>
2024-01-14 16:13:51 +0100

    fix projekname

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,36 +1,39 @@
-from src.ui_MainWindow import Ui_MainWindow
-from src.DialogProjectName import DialogProjectName
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtCore import Qt
+from src.ui_MainWindow import Ui_MainWindow
+from src.DialogProjectName import DialogProjectName
+from src.config import conf
 
 
 
-""" Klasse die das Hauptfenster erzeugt """
+'''Klasse die das Hauptfenster erzeugt'''
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.setWindowFlag(Qt.FramelessWindowHint) 
+        self.labelProjektname.setText(conf.value('lastProject/name'))
+        
         #self.showFullScreen()
-
-        """dialog fenster projektname"""
+        
+        '''dialog fenster projektname'''
         self.pushButtonProjektname.clicked.connect(self.openDialogProjectName)
         self.dialogProjectName = DialogProjectName(self)
         
-        """m² Resetbutton"""
+        '''m² Resetbutton'''
         self.m2pushButton.clicked.connect(self.M2ResetClicked)
         
 
-    """Methode zum öffnen des Dialogs zum editieren des Projektnamens"""
+    '''Methode zum öffnen des Dialogs zum editieren des Projektnamens'''
     def openDialogProjectName(self):
         self.dialogProjectName.show()
 
 
-    """m² Resetbutton clicked"""   
+    '''m² Resetbutton clicked'''   
     def M2ResetClicked(self):
         self.resetM2Lcd()
 
 
-    """setze m² LCD zurück"""
+    '''setze m² LCD zurück'''
     def resetM2Lcd(self):
         self.m2lcdNumber.display(0)
